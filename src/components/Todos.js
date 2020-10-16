d0dec2351e5bdf105c6ae578931028795ac5eb18
Aghiad Alzein <aghiad.alzein@gmail.com>
2020-10-17 00:12:07 +0300

    react modification the original app

---
+++ src/components/Todos.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Todos extends React.Component{
+    render(){
+        return(
+        <div>
+            <h2>Todos Page</h2>
+        </div>
+
+        )
+    }
+}
+
+export default Todos
