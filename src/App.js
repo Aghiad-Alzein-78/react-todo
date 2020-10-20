32eae565da1e3345556e6b580dd2b94a78836125
Aghiad Alzein <aghiad.alzein@gmail.com>
2020-10-20 20:19:53 +0300

    Finish add todo to the state

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Todos from './components/Todos'
 import './App.css';
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
-
+var my_id=4
 class App extends React.Component {
+ 
   state = {
     todos:[
       {id:1,title:"Take out the trash",completed:false},
@@ -32,18 +33,28 @@ class App extends React.Component {
       todos:todosCopy
     })
   }
- 
+  
+ addTodo=(title)=>{
+  let todosList=this.state.todos
+  todosList.push({id:my_id,title:title,completed:false})
+  my_id++
+  console.log(todosList)
+  this.setState({
+    todos:todosList
+  })
+ }
   render(){
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos 
             todos={this.state.todos} 
             markComplete={this.markComplete} 
             delTodo={this.delTodo}
           />
+
         </div>
       </div>
     );
